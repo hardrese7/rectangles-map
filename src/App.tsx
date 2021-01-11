32914b7ae2346cf17f21afc1640d217090d4e181
Rodion Golovushkin <hard.rese7@gmail.com>
2021-01-12 00:22:36 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TheMap from './components/TheMap';
+
+function App() {
+  return (
+    <div className="App">
+      <TheMap />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TheMap.tsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiaGFyZHJlc2U3IiwiYSI6ImNranNxc2h2OTA2ZTgyeXFzZmY5N3R0Z2gifQ.tkKZnAhtf0ZlA3lhD6j-cg'; // TODO move to config
+const DEFAULT_MAP_SETTINGS = {
+  lng: 5,
+  lat: 34,
+  zoom: 2,
+};
+
+function TheMap() {
+  const [, setMap] = useState<mapboxgl.Map | null>(null);
+  const mapContainerRef = useRef(null);
+  useEffect(() => {
+    if (!mapContainerRef) {
+      return;
+    }
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current! as HTMLElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [DEFAULT_MAP_SETTINGS.lng, DEFAULT_MAP_SETTINGS.lat],
+      zoom: DEFAULT_MAP_SETTINGS.zoom,
+    });
+    setMap(map);
+    // TODO clear memory
+  }, [mapContainerRef]);
+  return (
+    <div>
+      <div ref={mapContainerRef} className="mapContainer" />
+    </div>
+  );
+}
+
+export default TheMap;
